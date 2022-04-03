7f5858a6f15243c1f20c5f7cfe48523d38b65fd5
SpaceCadetVulture <101519696+SpaceCadetVulture@users.noreply.github.com>
2022-04-03 13:57:32 +0200

    Add files via upload

---
+++ simple-js-app/js/scripts.js
@@ -0,0 +1,1 @@
+let pokemonList = [{name: 'Pikachu', height: 3, type: ['thunder', 'grass']  },{name: 'Charmander', height: 5, type:['fire', 'flying']},{name: 'Blastoise', height: 20, type:['water', 'strength']}];
