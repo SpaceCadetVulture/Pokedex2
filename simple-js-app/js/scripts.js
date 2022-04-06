df44d3b3e3b3cbe8e837981ac35586db835dd630
SpaceCadetVulture <Vito.Santiago@protonmail.com>
2022-04-06 21:40:59 +0200

    Update scripts.js

--- simple-js-app/js/scripts.js
+++ simple-js-app/js/scripts.js
@@ -1,1 +1,10 @@
-let pokemonList = [{name: 'Pikachu', height: 3, type: ['thunder', 'grass']  },{name: 'Charmander', height: 5, type:['fire', 'flying']},{name: 'Blastoise', height: 20, type:['water', 'strength']}];
+let pokemonList = [{name: 'Pikachu', height: 3, type: ['thunder', 'grass']  },
+{name: 'Charmander', height: 5, type:['fire', 'flying']},
+{name: 'Blastoise', height: 20, type:['water', 'strength']}];
+
+
+for (;pokemonList[i];) {
+  pokemonList = pokemonList[i];
+  i++;
+}
+document.write(pokemonList)
