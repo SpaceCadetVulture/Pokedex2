6b2ce0fe83583896bf53129c6aa2663911cd4108
SpaceCadetVulture <Vito.Santiago@protonmail.com>
2022-04-07 20:19:23 +0200

    updates code
    Updated code to show the Pokemon correctly

--- simple-js-app/js/scripts.js
+++ simple-js-app/js/scripts.js
@@ -3,28 +3,28 @@ let pokemonList = [{name: 'Pikachu', height: 3, type: ['thunder', 'grass']  },
 {name: 'Blastoise', height: 20, type:['water', 'strength']}];
 
 
-for (;pokemonList[i];) {
-  pokemonList = pokemonList[i];
+for (let i = 0; i < pokemonList.length; ) {
+ document.write(pokemonList[i].name + " ");
+ console.log("random")
   i++;
 }
-document.write(pokemonList)
 
-let repository = [
-   {
-    name:"Pikachu"
-    height: 3
-    type:['thunder','grass']
-  },
-    {
-     name:"Charmander"
-     height: 5
-     type:['fire','flying']
-   },
-     {
-      name:"Blastoise"
-      height: 20
-      type:['water','strength']
-    }
-  ];
+// let repository = [
+//    {
+//     name:"Pikachu"
+//     height: 3
+//     type:['thunder','grass']
+//   },
+//     {
+//      name:"Charmander"
+//      height: 5
+//      type:['fire','flying']
+//    },
+//      {
+//       name:"Blastoise"
+//       height: 20
+//       type:['water','strength']
+//     }
+//   ];
 
-  if (pokemon.height)
+  //if (pokemon.height)
