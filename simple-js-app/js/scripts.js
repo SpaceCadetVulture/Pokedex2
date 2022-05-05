7e950160537830fdbba7ad7dd60db625b28f53e3
SpaceCadetVulture <Vito.Santiago@protonmail.com>
2022-05-05 19:47:21 +0200

    changed the code
    changed the code with the help of tutor

--- simple-js-app/js/scripts.js
+++ simple-js-app/js/scripts.js
@@ -1,48 +1,3 @@
-let pokemonList = [{name: 'Pikachu', height: 3, type: ['thunder', 'grass']  },
-{name: 'Charmander', height: 5, type:['fire', 'flying']},
-{name: 'Blastoise', height: 20, type:['water', 'strength']}];
-
-
-for (let i = 0; i < pokemonList.length; ) {
- document.write(pokemonList[i].name + " ");
-   document.write("<br>");
-   document.write("<br>");
- console.log("random")
-  i++;
-}
-
-let repository = [
-   {
-    name:"Pikachu",
-    height: 3,
-    type:['thunder','grass']
-  },
-    {
-     name:"Charmander",
-     height: 5,
-     type:['fire','flying']
-   },
-     {
-      name:"Blastoise",
-      height: 20,
-      type:['water','strength']
-    }
-  ];
-
-  for (let i = 0; i < repository.length; ) {
-  if (repository[i].height > 5) {
-    // document.write(repository[i].name + ' - ' + "Wow, that's big!");
-  document.write(repository[i].name + ' (height: ' + repository[i].height + ') ' + ' - ' + "Wow, that's big!");
-  document.write("<br>");
-  document.write("<br>")
-  }else {
-    document.write(repository[i].name + ' (height: ' + repository[i].height + ') ');
-      document.write("<br>");
-      document.write("<br>")
-  }
-
-    i++;
-  }
 
 
 let pokemonRepository= (function(){
@@ -64,10 +19,6 @@ let pokemonRepository= (function(){
     type:['water', 'strength']
     }];
 
-  nameList.forEach(function(pokemonList) {
-  console.log(pokemonList);
-});
-
 
 function add(pokemon){
         pokemonList.push(pokemon);
@@ -85,5 +36,24 @@ function add(pokemon){
 
    })();
 
-   pokemonRepository.add({ name: 'Pikachu', height: 3, type: ['thunder', 'grass']});
+   pokemonRepository.add({
+     name: 'Pikachu',
+     height: 3,
+     type: ['thunder', 'grass'], });
    console.log(pokemonRepository.getAll());
+
+const pokemon = pokemonRepository.getAll()
+
+pokemons.forEach((element) => {
+  if (element.height > 5)
+});
+
+   document.write(repository[i].name + ' (height: ' + repository[i].height + ') ' + ' - ' + "Wow, that's big!");
+   document.write("<br>");
+   document.write("<br>")
+   }else {
+     document.write(repository[i].name + ' (height: ' + repository[i].height + ') ');
+       document.write("<br>");
+       document.write("<br>")
+
+  console.log(pokemons);
